2f4e5df6dcc8b49eb82f5887a1b2200fa9973cdf
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-14 17:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function Print(n){
+	if(n%3===0 && n%5!=0){
+		alert("Fizz");
+	}
+	else if(n%3!==0 && n%5===0){
+		alert("Buzz");
+	}
+	else if(n%3===0 && n%5===0){
+		alert("FizzBuzz");
+	}
+}
